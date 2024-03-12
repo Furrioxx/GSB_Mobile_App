466e4cc55b9ae35c5cf6a5d7e178ea0d7a4000cc
Furrioxx <pelud.tom1@gmail.com>
2024-03-12 17:59:34 +0100

    add listener nav bar on CostDetailActivity

--- app/src/main/java/com/furrioxx/connexiongsb/activities/CostDetailActivity.java
+++ app/src/main/java/com/furrioxx/connexiongsb/activities/CostDetailActivity.java
@@ -1,9 +1,11 @@
 package com.furrioxx.connexiongsb.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -11,12 +13,15 @@ import com.furrioxx.connexiongsb.R;
 import com.furrioxx.connexiongsb.async.GetCost;
 import com.furrioxx.connexiongsb.async.GetCostSheet;
 import com.furrioxx.connexiongsb.entity.User;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class CostDetailActivity extends AppCompatActivity {
 
     private User user;
+    private String idFicheFrais;
     private TextView titleCostDetail,titleTotalCostTv, titleRefundCostTv, titleStateCostSheetTv;
     private LinearLayout linearLayoutCostDetail;
+    private BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,14 +33,49 @@ public class CostDetailActivity extends AppCompatActivity {
         titleRefundCostTv = findViewById(R.id.titleRefundTotal);
         titleStateCostSheetTv = findViewById(R.id.titleStateCostSheet);
 
+        //bottom navigation
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        //set wich item is selected
+        bottomNavigationView.setSelectedItemId(R.id.home);
 
         Intent intent = getIntent();
-        String idFicheFrais = intent.getStringExtra(GetCostSheet.ID_FICHE_FRAIS);
-        user = intent.getParcelableExtra("user");
+        if (intent != null){
+            //récupération de l'utilisateur
+            user = intent.getParcelableExtra("user");
+            if (user != null){
+                idFicheFrais = intent.getStringExtra(GetCostSheet.ID_FICHE_FRAIS);
+                titleCostDetail.append(idFicheFrais);
+                initialize();
+            }
+        }
 
-        titleCostDetail.append(idFicheFrais);
-
+    }
+    private void initialize(){
         String[] param = {user.getMail() , user.getToken(), idFicheFrais};
         new GetCost(this, linearLayoutCostDetail ,titleTotalCostTv, titleRefundCostTv, titleStateCostSheetTv).execute(param);
+
+        //création des listener de la navigation
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                int itemId = item.getItemId();
+                if (itemId == R.id.add) {
+                    startActivity(new Intent(getApplicationContext(), AddCostSheetActivity.class).putExtra("user", user));
+                    overridePendingTransition(0,0);
+                    return true;
+                } else if (itemId == R.id.home) {
+                    startActivity(new Intent(getApplicationContext(), DashboardVisitorActivity.class).putExtra("user", user));
+                    overridePendingTransition(0,0);
+                    return true;
+                } else if (itemId == R.id.profil) {
+                    startActivity(new Intent(getApplicationContext(), ProfileActivity.class).putExtra("user", user));
+                    overridePendingTransition(0,0);
+                    return true;
+                }
+                return false;
+            }
+        });
     }
 }
