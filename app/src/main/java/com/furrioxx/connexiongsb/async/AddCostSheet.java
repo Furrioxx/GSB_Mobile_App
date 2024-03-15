e1cb1afc6dbc0107963941a606182d087ecdd7bc
Furrioxx <pelud.tom1@gmail.com>
2024-03-15 14:04:38 +0100

    add asynk task add cost sheet

---
+++ app/src/main/java/com/furrioxx/connexiongsb/async/AddCostSheet.java
@@ -0,0 +1,15 @@
+package com.furrioxx.connexiongsb.async;
+
+import android.os.AsyncTask;
+
+public class AddCostSheet extends AsyncTask<String, Void, String> {
+    @Override
+    protected String doInBackground(String... strings) {
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+    }
+}
